678b285fe743430d8869274f98e327cf0ea10325
Hugh Byrne <67772139+hughbyrne10@users.noreply.github.com>
2023-04-25 13:15:15 +0100

    Update Day 021. XNOR gate is now XOR gate. Parameter SEED added to code.

--- Day_021_lfsr/day021_lfsr.sv
+++ Day_021_lfsr/day021_lfsr.sv
@@ -3,7 +3,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module day021_lfsr(
+module day021_lfsr#(parameter SEED = 4'hF)(
     input logic clk_i,
     input logic n_rst_i,
     input logic enable_i,
@@ -12,15 +12,15 @@ module day021_lfsr(
 
 reg [3:0] lfsr;
 initial begin
-lfsr = 4'h0;
+lfsr = SEED;
 end
 always @(posedge clk_i)
     if (n_rst_i == 1'b0)
-        lfsr <= 4'h00;
+        lfsr <= SEED;
     else if (enable_i)
         begin
             lfsr[3:1] <= lfsr[2:0];
-            lfsr[0] <= ~^lfsr[3:2];
+            lfsr[0] <= ^lfsr[3:2];
         end
         
 assign lfsr_o = lfsr;
